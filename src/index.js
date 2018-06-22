c1fbc67b07f248617b14301427b1392034cb2d6a
livelybone <2631541504@qq.com>
2018-06-22 18:00:58 +0800

    build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const SlideForMoreBase = require('./component/SlideForMoreBase.vue').default;
 const SlideForMore = require('./component/SlideForMore.vue').default;
+
 module.exports.SlideForMoreBase = SlideForMoreBase;
 module.exports.SlideForMore = SlideForMore;
